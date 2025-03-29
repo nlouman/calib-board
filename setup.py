ff6e481b3d644a860ff9076b70be7db79e8eef59
Nino <nino.louman@gmail.com>
2025-03-29 16:40:53 +0100

    save state

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='calib_board',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'calibrate-extrinsics = calib_board.scripts.run:main'
+        ],
+    },
 )
